5a00405bdff63dc0b518ee81fb7442bd04b8e97c
amna-kamran <amnakamran139@gmail.com>
2024-01-15 20:36:17 +0500

    self portrait added

--- components/About.js
+++ components/About.js
@@ -1,10 +1,11 @@
+import { Fade } from "react-bootstrap";
 import styles from "../styles/About.module.css";
 import FadeIn from "./FadeIn";
 const About = () => {
   const languages = ["javascript ES6+", "dart", "python", "java"];
   const frameworks = ["reactJS", "nextjs", "react native", "flutter"];
   return (
-    <div id="about">
+    <div id="about" className={styles.main_container}>
       <FadeIn>
         <div className="padding">
           <h3 className="heading">/about me</h3>
@@ -48,6 +49,11 @@ const About = () => {
             When i'm not coding, i do digital art, crochet and also play video
             games.
           </p>
+        </div>
+      </FadeIn>
+      <FadeIn>
+        <div className={styles.picture}>
+          <img src="/self-portrait.png"></img>
         </div>
       </FadeIn>
     </div>
